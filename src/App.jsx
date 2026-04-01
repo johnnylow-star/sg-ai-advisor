77742e5bb7d26c9d9a461d30ca8e4ffdff834766
Johnny Low <johnny.low@saia.org.sg>
2026-04-01 14:05:51 +0800

    Fix import casing to match git filenames

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Form from './pages/Form'
 import Results from './pages/Results'
 
